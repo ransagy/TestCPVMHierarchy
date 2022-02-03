9ec535287227a80b7b60523886dd321eb952d5d2
Ran Sagy <ran@wsc-sports.com>
2022-02-03 15:22:40 +0200

    Initial Commit

---
+++ Console/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine(typeof(Newtonsoft.Json.JsonToken).Assembly.FullName);
+Console.WriteLine(typeof(Newtonsoft.Json.Linq.JsonSelectSettings).Assembly.FullName);
---
+++ Console2/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(typeof(Newtonsoft.Json.JsonToken).Assembly.FullName);
